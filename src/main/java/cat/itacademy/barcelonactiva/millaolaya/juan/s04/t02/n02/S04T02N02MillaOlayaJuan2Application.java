63469e1420d88c790181afeeb3089a579780d279
tokyumilla <tokyumilla@gmail.com>
2023-04-21 20:58:57 +0200

    Creando proyecto maven

---
+++ src/main/java/cat/itacademy/barcelonactiva/millaolaya/juan/s04/t02/n02/S04T02N02MillaOlayaJuan2Application.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.millaolaya.juan.s04.t02.n02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S04T02N02MillaOlayaJuan2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S04T02N02MillaOlayaJuan2Application.class, args);
+	}
+
+}
