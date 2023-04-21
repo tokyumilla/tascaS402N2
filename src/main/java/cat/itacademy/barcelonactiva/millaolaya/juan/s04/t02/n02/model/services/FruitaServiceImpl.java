b6b5f5a1fed174596acfd2ed7d72e9164b795f0a
tokyumilla <tokyumilla@gmail.com>
2023-04-21 21:29:06 +0200

    Corregido error @Service

--- src/main/java/cat/itacademy/barcelonactiva/millaolaya/juan/s04/t02/n02/model/services/FruitaServiceImpl.java
+++ src/main/java/cat/itacademy/barcelonactiva/millaolaya/juan/s04/t02/n02/model/services/FruitaServiceImpl.java
@@ -3,10 +3,12 @@ package cat.itacademy.barcelonactiva.millaolaya.juan.s04.t02.n02.model.services;
 import cat.itacademy.barcelonactiva.millaolaya.juan.s04.t02.n02.model.domain.Fruita;
 import cat.itacademy.barcelonactiva.millaolaya.juan.s04.t02.n02.model.repository.FruitaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class FruitaServiceImpl implements FruitaService{
 
     @Autowired
